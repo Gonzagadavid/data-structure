b3962138e11494e8fc02cb88276ca5c937cb8261
gonzagadavid <david.blesses@hotmail.com>
2023-09-12 19:46:38 -0300

    feat: add fila_ecadeada

---
+++ fila_encadeada/NodeType.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 12/09/23.
+//
+
+#ifndef ESTRUTURA_DE_DADOS_NODETYPE_H
+#define ESTRUTURA_DE_DADOS_NODETYPE_H
+
+#endif //ESTRUTURA_DE_DADOS_NODETYPE_H
+
+typedef char ItemType;
+
+struct NodeType
+{
+    ItemType info;
+    NodeType* next;
+};
